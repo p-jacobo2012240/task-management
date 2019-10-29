cea98ddc946aedc14a4f28f8e43dd17813ffa731
p-jacobo2012240 <pablojacobo1998@gmail.com>
2019-10-28 22:43:37 -0600

    Session ENV -common

--- src/java/org/pablojacobo/controllers/OAuthController.java
+++ src/java/org/pablojacobo/controllers/OAuthController.java
@@ -19,7 +19,8 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet(name = "OAuthController", urlPatterns = {"/OAuthController"})
 public class OAuthController extends HttpServlet {
-
+    private String tempUsername = "";
+    private String tempPasssword = "";
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
      * methods.
@@ -34,15 +35,8 @@ public class OAuthController extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet OAuthController</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet OAuthController at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+            this.tempUsername = request.getParameter("username");
+            this.tempPasssword = request.getParameter("pass");
         }
     }
 
---
+++ web/dashboard.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : dashboard
+    Created on : 28-oct-2019, 22:29:36
+    Author     : Hackobo
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>

f3c42b4a0a1396621be4ec9a92cb18906cecf6ed
p-jacobo2012240 <pablojacobo1998@gmail.com>
2022-03-23 22:50:43 -0600

    configuration for hexagonal architecture

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ApplicationModule } from './application/application/application.module';
+import { DomainModule } from './domain/domain/domain.module';
+import { InfrastructureModule } from './infrastructure/infrastructure/infrastructure.module';
 
 @Module({
-  imports: [],
+  imports: [ApplicationModule, DomainModule, InfrastructureModule],
 })
 export class AppModule {}

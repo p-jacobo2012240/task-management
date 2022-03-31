7ec54a61556c52fa280ea31dcf0d715c4c68050d
p-jacobo2012240 <pablojacobo1998@gmail.com>
2022-03-31 00:43:16 -0600

    add basics services

--- src/infrastructure/controllers/task/task.controller.ts
+++ src/infrastructure/controllers/task/task.controller.ts
@@ -1,16 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateTaskDto } from 'src/infrastructure/dto/create-task.dto';
+import { Task } from 'src/infrastructure/entities/task.entity';
 import { TaskService } from 'src/infrastructure/services/task/task.service';
 
-@Controller('task')
+@Controller('tasks')
 export class TaskController {
 
     constructor(
         private taskService: TaskService
     ) {}
 
-    @Get('/dummy')
-    getAllTasks() : string[] {
+    @Get()
+    getAllTasks() : Task[] {
         return this.taskService.getAllTasks()
+    }
+
+    @Post()
+    createNewTask(@Body() createTaskDto: CreateTaskDto) : Task {
+        return this.taskService.createTask(createTaskDto);
     }
 
 }
--- src/infrastructure/services/task/task.service.ts
+++ src/infrastructure/services/task/task.service.ts
@@ -1,11 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { CreateTaskDto } from 'src/infrastructure/dto/create-task.dto';
+import { TaskStatus } from 'src/infrastructure/entities/task-status';
+import { Task } from 'src/infrastructure/entities/task.entity';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
 export class TaskService {
     
-    private taskList = [ 'prepare coffee',  'make some code', 'spend some money in food']
+    private taskList: Task[] = [];
 
-    getAllTasks(){
+    getAllTasks(): Task[] {
         return this.taskList;
     }
+
+    createTask(createTaskDto: CreateTaskDto) : Task {
+        const { title, description } = createTaskDto;
+
+        const newTask: Task = {
+            id: uuid(),
+            title: title,
+            description: description,
+            status: TaskStatus.OPEN
+        }
+        this.taskList.push(newTask);
+        return newTask;
+    } 
 }

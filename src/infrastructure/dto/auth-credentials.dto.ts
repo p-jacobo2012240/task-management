961b3cd21d8ffe6e431e3bd968f3196677ec5cbd
p-jacobo2012240 <pablojacobo1998@gmail.com>
2022-05-18 00:08:52 -0600

    added password validation

--- src/infrastructure/dto/auth-credentials.dto.ts
+++ src/infrastructure/dto/auth-credentials.dto.ts
@@ -1,7 +1,17 @@
+import { IsString, Matches, MaxLength, MinLength } from "class-validator";
 
 export class AuthCredentials {
 
+    @IsString()
+    @MinLength(4)
+    @MaxLength(20)
     username: string;
 
+    @IsString()
+    @MinLength(8)
+    @MaxLength(32)
+    @Matches(/(?:(?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, { 
+        message: 'password is to weak' 
+    })
     password: string;
 }
